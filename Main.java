1fdead91ead171caffd57124e54567009ac0b294
GKavetskij <glebkavetskii@gmail.com>
2026-09-20 16:57:59 +0300

    Add files via upload
    Kavetskiy Gleb lb01 Task 12

--- Main.java
+++ Main.java
@@ -13,28 +13,18 @@ public class Main {
             sequence[i] = scanner.nextInt();
         }
 
-        boolean found = false;
-
         for (int n : sequence) {
             String s = String.valueOf(Math.abs(n));
-            if (s.length() % 2 != 0) continue;
+            int count = 0;
 
-            found = true;
-            int[] counts = new int[10];
-            for (char c : s.toCharArray()) {
-                if (Character.isDigit(c)) {
-                    counts[c - '0']++;
+            for (int i = 0; i < s.length(); i++) {
+                int digit = s.charAt(i) - '0';
+                if (digit < 5) {
+                    count++;
                 }
             }
-            System.out.println("Число: " + n + " (цифр: " + s.length() + ")");
-            for (int d = 0; d < 10; d++) {
-                if (counts[d] > 0) {
-                    System.out.println("  цифра " + d + " встречается " + counts[d] + " раз");
-                }
-            }
-        }
-        if (!found) {
-            System.out.println("В последовательности нет чисел с чётным количеством цифр.");
+
+            System.out.println("Число: " + n + " -> цифр меньше 5: " + count);
         }
 
         scanner.close();
